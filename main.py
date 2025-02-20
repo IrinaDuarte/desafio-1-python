e924d7e7a2f196beb740f31bdccfce01b07e3477
IrinaDuarte <irinamottacduarte@gmail.com>
2025-02-20 17:15:35 -0300

    terceira atualização

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ else:
 
 # missão 3
 
-nota = 89
+nota = 99
 
-if nota >= 90:
+if nota >= 90 and nota <= 100:
     print('Parabéns, você tirou A')
-elif nota >= 80:
+elif nota >= 80 and nota <= 89:
     print('Muito bem, você tirou B.')
-elif nota >= 70:
+elif nota >= 70 and nota <= 79:
     print('Bom trabalho, você tirou C.')
-elif nota >= 60:
+elif nota >= 60 and nota <= 69:
     print('Fique atento, você tirou D.')
 else:
     print('Estude um pouco mais, você tirou F.')
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,12 @@
-def contagem_caracteres(nome):
-    return f'O nome {nome} tem {len(nome)} letras'
+nota = 99
 
-
-print(contagem_caracteres('Ana'))
+if nota >= 90 and nota <= 100:
+    print('Parabéns, você tirou A')
+elif nota >= 80 and nota <= 89:
+    print('Muito bem, você tirou B.')
+elif nota >= 70 and nota <= 79:
+    print('Bom trabalho, você tirou C.')
+elif nota >= 60 and nota <= 69:
+    print('Fique atento, você tirou D.')
+else:
+    print('Estude um pouco mais, você tirou F.')
